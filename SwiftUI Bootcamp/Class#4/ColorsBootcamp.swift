f03464a89fd70b91a34b763b5c574de73c3e5933
jgestradam <jgestradam@unal.edu.co>
2022-11-01 23:51:44 -0500

    Class#4

---
+++ SwiftUI Bootcamp/Class#4/ColorsBootcamp.swift
@@ -0,0 +1,28 @@
+//
+//  ColorsBootcamp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by Joseph Estrada on 11/1/22.
+//
+
+import SwiftUI
+
+struct ColorsBootcamp: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25.0)
+            .fill(
+                //Color.primary
+                //Color(UIColor.secondarySystemBackground)
+                Color("CustomColor")
+            )
+            .frame(width: 300, height: 200)
+            //.shadow(radius: 10)
+            .shadow(color: Color("CustomColor").opacity(0.3), radius: 10, x: -20, y: -20)
+    }
+}
+
+struct ColorsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorsBootcamp()
+    }
+}
