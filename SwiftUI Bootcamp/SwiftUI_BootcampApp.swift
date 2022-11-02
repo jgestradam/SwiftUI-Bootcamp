63f7af3feed70456062ea7650d21331f6b4fa97a
jgestradam <jgestradam@unal.edu.co>
2022-11-01 21:12:04 -0500

    Initial Commit

---
+++ SwiftUI Bootcamp/SwiftUI_BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BootcampApp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by Joseph Estrada on 11/1/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
