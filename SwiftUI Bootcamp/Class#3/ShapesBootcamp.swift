9c131c103f111345332e27ca7001ce9ba151406f
jgestradam <jgestradam@unal.edu.co>
2022-11-01 22:20:44 -0500

    Class#3

---
+++ SwiftUI Bootcamp/Class#3/ShapesBootcamp.swift
@@ -0,0 +1,33 @@
+//
+//  ShapesBootcamp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by Joseph Estrada on 11/1/22.
+//
+
+import SwiftUI
+
+struct ShapesBootcamp: View {
+    var body: some View {
+        //Circle()
+        //Ellipse()
+        //Capsule(style: .circular)
+        //Rectangle()
+        RoundedRectangle(cornerRadius: 10)
+            //.fill(.blue)
+            //.foregroundColor(.pink)
+            //.stroke()
+            //.stroke(Color.red)
+            //.stroke(Color.blue, lineWidth: 30)
+            //.stroke(Color.orange, style: StrokeStyle(lineWidth: 30, lineCap: .round, dash: [30]))
+            .trim(from: 0.4, to: 1.0)
+            //.stroke(Color.purple, lineWidth: 50)
+            .frame(width: 300, height: 200)
+    }
+}
+
+struct ShapesBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ShapesBootcamp()
+    }
+}
