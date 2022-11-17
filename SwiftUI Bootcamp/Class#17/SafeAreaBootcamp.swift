fb807a3b18c9763265a3a6f4900c388c762d4f0c
jgestradam <jgestradam@unal.edu.co>
2022-11-17 18:04:53 -0500

    Class#17

---
+++ SwiftUI Bootcamp/Class#17/SafeAreaBootcamp.swift
@@ -0,0 +1,54 @@
+//
+//  SafeAreaBootcamp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by Joseph Estrada on 11/17/22.
+//
+
+import SwiftUI
+
+struct SafeAreaBootcamp: View {
+    var body: some View {
+        ScrollView {
+            VStack {
+                Text("Title goes here")
+                    .font(.largeTitle)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                ForEach(0..<10) { INDEX in
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .fill(Color.white)
+                        .frame(height: 150)
+                        .shadow(radius: 10)
+                        .padding(20)
+                }
+            }
+        }
+        //.background(Color.blue)
+        .background(
+            Color.red
+            .edgesIgnoringSafeArea(.all)
+        )
+//        ZStack {
+//
+//            //background
+//            Color.blue
+//                .edgesIgnoringSafeArea(.all)
+//            //foreground
+//
+//            VStack {
+//                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//                Spacer()
+//            }
+//            .frame(maxWidth: .infinity, maxHeight: .infinity)
+//            //.background(Color.red)
+//            //.edgesIgnoringSafeArea(.all)
+//        }
+    }
+}
+
+struct SafeAreaBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        SafeAreaBootcamp()
+    }
+}
