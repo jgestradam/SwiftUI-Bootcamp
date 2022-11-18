a518a4aaeb6f83ae18edcf64f93ded7999727027
jgestradam <jgestradam@unal.edu.co>
2022-11-18 14:49:14 -0500

    Class#25

---
+++ SwiftUI Bootcamp/Class#25/AnimationBootcamp.swift
@@ -0,0 +1,44 @@
+//
+//  AnimationBootcamp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by Joseph Estrada on 11/18/22.
+//
+
+import SwiftUI
+
+struct AnimationBootcamp: View {
+    @State var isAnimated: Bool = false
+    var body: some View {
+        VStack {
+            Button("Button") {
+                isAnimated.toggle()
+            }
+            
+            Spacer()
+            
+            RoundedRectangle(cornerRadius: isAnimated ? 50 : 25)
+                .fill(isAnimated ? Color.red : Color.green)
+                .frame(
+                    width: isAnimated ? 100 : 300,
+                    height: isAnimated ? 100 : 300
+                )
+                .rotationEffect(Angle(degrees: isAnimated ? 360 : 0))
+                .offset(y: isAnimated ? 300 : 0)
+                .animation(
+                    Animation
+                        .default
+                        .repeatForever(autoreverses: true)
+                )
+            
+            
+            Spacer()
+        }
+    }
+}
+
+struct AnimationBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimationBootcamp()
+    }
+}
