d0032788014cd481e54741b8e907c12978ec252a
jgestradam <jgestradam@unal.edu.co>
2022-11-22 17:52:31 -0500

    Class#41

---
+++ SwiftUI Bootcamp/Class#41/StepperBootcamp.swift
@@ -0,0 +1,43 @@
+//
+//  StepperBootcamp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by Joseph Estrada on 11/22/22.
+//
+
+import SwiftUI
+
+struct StepperBootcamp: View {
+    
+    @State var stepperValue: Int = 10
+    @State var widthIncrement: CGFloat = 0
+    
+    var body: some View {
+        VStack {
+            Stepper("Stepper: \(stepperValue)", value: $stepperValue)
+                .padding(50)
+            
+            RoundedRectangle(cornerRadius: 25.0)
+                .frame(width: 100 + widthIncrement, height: 100)
+            
+            Stepper("Stepper 2") {
+                //increment
+                incrementWidth(amount: 100)
+            } onDecrement: {
+                //decrement
+                incrementWidth(amount: -100)
+            }
+        }
+    }
+    func incrementWidth(amount: CGFloat) {
+        withAnimation(.easeInOut) {
+            widthIncrement += amount
+        }
+    }
+}
+
+struct StepperBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        StepperBootcamp()
+    }
+}
