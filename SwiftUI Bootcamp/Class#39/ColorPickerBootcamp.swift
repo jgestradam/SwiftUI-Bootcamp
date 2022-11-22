88e5feca14b191da6eb630078105c46fb34092cd
jgestradam <jgestradam@unal.edu.co>
2022-11-22 11:52:35 -0500

    Class#39

---
+++ SwiftUI Bootcamp/Class#39/ColorPickerBootcamp.swift
@@ -0,0 +1,38 @@
+//
+//  ColorPickerBootcamp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by Joseph Estrada on 11/22/22.
+//
+
+import SwiftUI
+
+struct ColorPickerBootcamp: View {
+    
+    @State var backgroundColor: Color = .green
+    
+    var body: some View {
+        ZStack {
+            backgroundColor
+                .edgesIgnoringSafeArea(.all)
+            
+            ColorPicker(
+                "Selected Color",
+                selection: $backgroundColor,
+                supportsOpacity: true
+            )
+            .padding()
+            .background(Color.black)
+            .cornerRadius(10)
+            .foregroundColor(.white)
+            .font(.headline)
+            .padding(50)
+        }
+    }
+}
+
+struct ColorPickerBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorPickerBootcamp()
+    }
+}
