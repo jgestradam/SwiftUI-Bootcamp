04060c7f479d3b9f4ddc00e61ca143e690648afb
jgestradam <jgestradam@unal.edu.co>
2022-11-24 18:19:24 -0500

    Class#52

---
+++ SwiftUI Bootcamp/Class#52/AppStorageBootcamp.swift
@@ -0,0 +1,34 @@
+//
+//  AppStorageBootcamp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by Joseph Estrada on 11/24/22.
+//
+
+import SwiftUI
+
+struct AppStorageBootcamp: View {
+    
+    @AppStorage("name") var currentUserName: String?
+    
+    var body: some View {
+        VStack(spacing: 20){
+            Text(currentUserName ?? "Add Name Here")
+            
+            if let name = currentUserName {
+                Text(name)
+            }
+            
+            Button("Save".uppercased()) {
+                let name = "Emily"
+                currentUserName = name
+            }
+        }
+    }
+}
+
+struct AppStorageBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        AppStorageBootcamp()
+    }
+}
