8cf070c94d62bdb62985906ddec8b16f15537e84
jgestradam <jgestradam@unal.edu.co>
2022-11-25 23:18:36 -0500

    Class#56

---
+++ SwiftUI Bootcamp/Class#56/TextSelectionBootcamp.swift
@@ -0,0 +1,21 @@
+//
+//  TextSelectionBootcamp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by Joseph Estrada on 11/25/22.
+//
+
+import SwiftUI
+
+struct TextSelectionBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .textSelection(.enabled)
+    }
+}
+
+struct TextSelectionBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TextSelectionBootcamp()
+    }
+}
